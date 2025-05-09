c8f2cf127f920c94dd6ae9618eb3caadef99543a
asad <apple@APPLEs-MacBook-Air.local>
2025-05-09 21:28:06 +0500

    Updates

--- main.py
+++ main.py
@@ -161,11 +161,12 @@ async def get_userid(username):
     data = f'route_urls[0]=%2F{username}%2F&routing_namespace=igx_www%24a%2487a091182d5bd65bcb043a2888004e09&__d=www&__user=0&__a=1&__req=9&__hs=20213.HYP%3Ainstagram_web_pkg.2.1...1&dpr=2&__ccg=POOR&__rev=1022515417&__s=pa6mmy%3Aqgmc8c%3A2dr157&__hsi=7501024739781509948&__dyn=7xe5WwlEnwn8K2Wmm1twpUnwgU7S6EdF8aUco38w5ux609vCwjE1EE2Cw8G11wBw5Zx62G3i1ywOwa90Fw4Hw9O0Lbwae4UaEW2G0AEco5G0zEnwhE0za2-azo7u1xwIwbS1LwTwKG1pg2Xwr86C1mwrd6goK10xKi2K7E5y4U4u0P8K6U16Usw&__csr=hL1NNIn9dsAYIAl9t8Ky96aG9BWWRsLitlIMyG-jKiBgxBzGGiF9kAeAz8RN2C9jGF9aAK9WWhbVpeBQESGV9eXVK-q5oSiqRyVogVHmjGbSiueVkfFVeWBypGy-uiaz4i9KehVkQeAGbKuUB2Z1a9ze-uu8QECq4EyWx9AzXLQfCyUiCCwxyUyq5801iTcw0KmfzU15o8o7Kl1_w4mU1CU46E5-04Xo0hRgapYw6C0y8lwaK8DG9wCwEhhwy0b5wauywgE19IEdU4i4okUZ3lg8ocNpAaBh8-aUuCwppQ10o7e0bJwjUe87k-u1DD-6NxkaCw0qRU1VU0_q&__hsdp=l0MMyTjdl_7h2FihcjoAAayVojJ396oigoho4u2WlZaEkgCnzxVCV2m1I50xzE7JAxK36dEw98y366lx57Kexm3i2a4S7U4m2O2e18wGxW4EdXwiE98nwkUG12wCyU0z-7o1DU10U9U3ew8W1Yxh0b6682xwvE2Gwcy3G2bg8o3dx-13of8W5E6Ci18GfwhU8U4C3UUizEbo&__hblpi=0fW0To3Xwfa2m8wjEhhXzElwlUdo4m2O2e18wGxW4EdXwiE98nwkUG12wCyU0z-7o1DU10U9U3ew8W1Yxh0b6682xwvE2Gwcy3G2bg8o3dx-13of8W5E6W1rwhU8U4C3UUizEbo&__hblpn=0mUeU4y0ZE3HwoUtz89EW19wZxu36UmxC2mm1GwBzEy9DxF0EwLwQzozx20iO3qi0um1vUfo15o4C0IE25wdy221NwPw9G6E4S0iO0b1yFo6i6olwZwXwBwBwwAwqE6a3W78bA48&__comet_req=7&fb_dtsg=NAcO-mMWDD318VJtOxuQgwjjiLwwtM9u2iMNLgfYCk5zOMC0hpBdeyg%3A17855905060073950%3A1746274676&jazoest=26363&lsd=awHBVkAcim8ZZGDUF0jLHX&__spin_r=1022515417&__spin_b=trunk&__spin_t=1746468418&__crn=comet.igweb.PolarisProfilePostsTabRoute'
 
     async with session.post(
-            'https://www.instagram.com/ajax/bulk-route-definitions/',
-            headers=headers,
-            data=data,
-            proxy=proxy_url
-        ) as response:
+        'https://www.instagram.com/ajax/bulk-route-definitions/',
+        headers=headers,
+        data=data,
+        proxy=proxy_url,
+        ssl=False
+    ) as response:
 
         response_text = await response.text()
 
@@ -176,7 +177,7 @@ async def get_userid(username):
                 'user_id': data['payload']['payloads'][f'/{username}/']['result']['exports']['rootView']['props']['id']
             }
         except:
-            return {}
+            return {'details': 'wrong username'}
 
 
 @app.get('/api/userinfo', tags=["Following"])
@@ -318,6 +319,10 @@ async def get_posts(
     request: Request,
     username: str
 ):
+    
+    userid_response = await get_userid(username)
+    if not 'user_id' in userid_response:
+        return userid_response, 400
 
     cookies = {
         'fbm_124024574287414': 'base_domain=.instagram.com',
@@ -377,7 +382,13 @@ async def get_posts(
         result = await response.json()
 
         edges = result['data']['xdt_api__v1__feed__user_timeline_graphql_connection']['edges']
-        cleaned_edges = [edge for edge in edges if not edge['node'].get('clips_metadata')]
+
+        cleaned_edges = []
+
+        userid = userid_response['user_id']
+        for edge in edges:
+            if userid not in edge['node']['timeline_pinned_user_ids']:
+                cleaned_edges.append(edge)
 
         result['data']['xdt_api__v1__feed__user_timeline_graphql_connection']['edges'] = cleaned_edges
         return result
